7a3358691ff27ca697fdb2d4a046228ff64db4e1
Storhn <fredcurinckx@MBP-de-Fred.home>
2024-08-09 11:58:11 +0100

    posts creation complete

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
@@ -12,4 +13,7 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
+Route::get('posts', [PostController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('posts'); 
 require __DIR__.'/auth.php';
